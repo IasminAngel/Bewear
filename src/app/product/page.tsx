2538083aee0bd3233fd007f6c12661f9f27a4ed9
IasminAngel <iasminangelwork@gmail.com>
2025-08-05 20:51:09 -0300

    chore: add eslint, prettier and shadcn setup

---
+++ src/app/product/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const ProductPage = () => {
+  return (
+    <div>
+      <h1>Product Page</h1>
+      <Button>Add to Cart</Button>
+    </div>
+  );
+};
+
+export default ProductPage;
